c3ce6824a9d0002e7e9866ea77c0970c8481f7df
darkdread <sakatagintoki9001@gmail.com>
2019-11-02 14:42:26 +0800

    added gamemanager, gamestate. gamestate uses enum flags, bitwise. deciding what kind of beat|drum to use, fail beat, etc.

--- Assets/Scripts/BeatmapGame.cs
+++ Assets/Scripts/BeatmapGame.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using OsuParsers;
 
 public class BeatmapGame : MonoBehaviour {
     
     public static BeatmapGame instance;
     public List<OsuParsers.Beatmaps.Objects.HitObject> hitObjects = new List<OsuParsers.Beatmaps.Objects.HitObject>();
 
-    public float songDuration;
-    public float songTimer;
+    public GameObject beatPrefab;
+
+    public int songDuration;
+    public int songTimer;
 
     void Awake(){
         if (instance == null){
@@ -21,10 +22,48 @@ public class BeatmapGame : MonoBehaviour {
 
     // Update is called once per frame
     void Update(){
-        if (GameManager.gameState == ~GameState.Started | GameState.Paused){
+        // Paused and Started.
+        //  011
+        // &011
+        //  ---
+        //  011 != 001
+
+        // Paused and !Started.
+        //  010
+        // &011
+        //  ---
+        //  010 != 001
+
+        // !Paused and Started.
+        //  001
+        // &011
+        //  ---
+        //  001 != 001
+
+        // !Paused and !Started.
+        //  000
+        // &011
+        //  ---
+        //  000 != 001
+        // print(GameManager.gameState & (GameState.Started | GameState.Paused));
+        if ((GameManager.gameState & (GameState.Started | GameState.Paused)) != (GameState.Started)){
             return;
         }
 
+        songTimer += (int) (Time.deltaTime * 1000);
 
+        if (songTimer >= songDuration){
+            OnSongEnd();
+            return;
+        }
+
+        // Spawn beat.
+        if (songTimer >= hitObjects[0].StartTime){
+            hitObjects.RemoveAt(0);
+        }
+    }
+
+    public void OnSongEnd(){
+        GameManager.Stop();
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using OsuParsers;
 using System.IO;
 
-[Flags]
 public enum GameState {
     Started = 0x01,
     Paused = 0x02
@@ -16,38 +14,66 @@ public class GameManager : MonoBehaviour {
     public static GameState gameState; 
     
     // https://github.com/mrflashstudio/OsuParsers.
-    public string beatmapRepository = "Assets/Beatmaps/";
+    public string beatmapRepository = @"Assets/Beatmaps/";
     private static List<OsuParsers.Beatmaps.Beatmap> beatmaps = new List<OsuParsers.Beatmaps.Beatmap>();
     
     void Awake(){
         if (instance == null){
             instance = this;
             DirectoryInfo info = new DirectoryInfo(beatmapRepository);
-            FileInfo[] fileInfo = info.GetFiles(".osu");
+            FileInfo[] fileInfo = info.GetFiles("*.osu", SearchOption.AllDirectories);
 
             foreach(FileInfo file in fileInfo){
-                print(file.FullName);
-                // OsuParsers.Beatmaps.Beatmap beatmap = OsuParsers.Decoders.BeatmapDecoder.Decode();
-                // beatmaps.Add(beatmap);
+                // print(file.FullName);
+
+                OsuParsers.Beatmaps.Beatmap beatmap = OsuParsers.Decoders.BeatmapDecoder.Decode(file.FullName);
+                beatmaps.Add(beatmap);
             }
+
+            StartBeatmap(beatmaps[0]);
         } else {
             Destroy(gameObject);
         }
     }
 
-    public void StartBeatmap(OsuParsers.Beatmap.Beatmap beatmap){
-        if (gameState == GameState.Started){
+    public void StartBeatmap(OsuParsers.Beatmaps.Beatmap beatmap){
+        if ((gameState & GameState.Started) == GameState.Started){
             return;
         }
 
         BeatmapGame.instance.hitObjects = beatmap.HitObjects;
-        // BeatmapGame.instance.songDuration = beatmap.
+        BeatmapGame.instance.songDuration = beatmap.HitObjects[beatmap.HitObjects.Count - 1].EndTime;
 
         gameState = gameState | GameState.Started;
     }
 
+    public static void Stop(){
+        gameState = gameState & ~GameState.Started;
+    }
+
     // Update is called once per frame
     void Update(){
+        // print((int) gameState);
         
+        if ((gameState & GameState.Started) == GameState.Started){
+
+            if (Input.GetKeyDown(KeyCode.Escape)){
+                Pause((gameState & GameState.Paused) != GameState.Paused);
+            }
+        }
+    }
+
+    public void Pause(bool toPause){
+        if (toPause){
+            gameState = gameState | GameState.Paused;
+        } else {
+            gameState = gameState ^ GameState.Paused;
+        }
+
+        if ((gameState & GameState.Paused) == GameState.Paused){
+            print("Paused");
+        } else {
+            print("Resumed");
+        }
     }
 }
