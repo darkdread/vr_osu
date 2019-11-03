157862d3fdd7ff37409cdf0c222867912b245bde
Walson Low <walson.low.portfolio@gmail.com>
2019-11-04 07:50:40 +0800

    new version

--- Assets/Scripts/BeatmapGame.cs
+++ Assets/Scripts/BeatmapGame.cs
@@ -2,22 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Drum {
+    Left,
+    Right
+}
+
+[RequireComponent(typeof(AudioSource))]
 public class BeatmapGame : MonoBehaviour {
     
     public static BeatmapGame instance;
     public List<OsuParsers.Beatmaps.Objects.HitObject> hitObjects = new List<OsuParsers.Beatmaps.Objects.HitObject>();
+    public List<Transform> beats = new List<Transform>();
 
     public GameObject beatPrefab;
+    public Transform beatHolder;
+    public Transform beatMarker;
 
+    public int beatSpawnDelay = -3000;
     public int songDuration;
     public int songTimer;
+
+    private int latestSpawnedBeat = 0;
+    private AudioSource audioSource;
 
     void Awake(){
         if (instance == null){
             instance = this;
+            audioSource = GetComponent<AudioSource>();
         } else {
             Destroy(gameObject);
         }
+    }
+
+    public void StartBeatmap(OsuParsers.Beatmaps.Beatmap beatmap){
+        print(string.Format("Starting beatmap: {0} {1}", beatmap.GeneralSection.AudioFilename, beatmap.MetadataSection.Version));
+        
+        // Check if it's osu!mania. If it is, prevent it from loading, because osu!mania has
+        // points that can start consecutively. Meaning, 3 keys at a single time.
+        if (beatmap.GeneralSection.Mode == OsuParsers.Enums.Ruleset.Mania){
+            print(string.Format("Oh shit I'm sorry, we don't support osu!mania."));
+            return;
+        }
+
+        hitObjects = beatmap.HitObjects;
+        songDuration = beatmap.HitObjects[beatmap.HitObjects.Count - 1].EndTime;
+        songTimer = 0;
+        latestSpawnedBeat = 0;
+
+        // songTimer = 10000;
+
+        // Spawn beats.
+        for(int i = 0; i < hitObjects.Count; i++){
+            OsuParsers.Beatmaps.Objects.HitObject hitObject = hitObjects[i];
+
+            if (hitObject.StartTime + beatSpawnDelay <= songTimer){
+                SpawnBeat(hitObject);
+            }
+        }
+
+        // Read mp3 file from local disk and convert byte to PCM. Look at MP3Sharp.
+        StartCoroutine(GameManager.GetBeatmapAudioClip(beatmap, PlayBeatmapSong));
+
+        // GameManager.gameState = GameManager.gameState | GameState.Started;
+    }
+
+    private void PlayBeatmapSong(AudioClip audioClip){
+        audioSource.clip = audioClip;
+        audioSource.time = songTimer;
+        audioSource.Play();
+
+        GameManager.gameState = GameManager.gameState | GameState.Started;
     }
 
     // Update is called once per frame
@@ -57,10 +111,56 @@ public class BeatmapGame : MonoBehaviour {
             return;
         }
 
-        // Spawn beat.
-        if (songTimer >= hitObjects[0].StartTime){
-            hitObjects.RemoveAt(0);
+        // Spawn beats.
+        for(int i = latestSpawnedBeat; i < hitObjects.Count; i++){
+            OsuParsers.Beatmaps.Objects.HitObject hitObject = hitObjects[i];
+
+            if (hitObject.StartTime + beatSpawnDelay <= songTimer){
+                SpawnBeat(hitObject);
+            }
         }
+
+        // Move beats.
+        for(int i = 0; i < beats.Count; i++){
+            Transform beat = beats[i];
+
+            beat.position += -Vector3.forward * Time.deltaTime * 10;
+        }
+
+        if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.Z)){
+            HitDrum(Drum.Left);
+        }
+    }
+
+    public void HitDrum(Drum drum){
+        // Get closest beat within song timer.
+        GameObject beat = GetClosestBeat();
+        print(beat);
+
+        beat.SetActive(false);
+    }
+
+    public GameObject GetClosestBeat(){
+        for(int i = 0; i < hitObjects.Count; i++){
+            OsuParsers.Beatmaps.Objects.HitObject hitObject = hitObjects[i];
+
+            if (hitObject.StartTime < songTimer){
+                continue;
+            }
+
+            return beats[i].gameObject;
+        }
+
+        return null;
+    }
+
+    private void SpawnBeat(OsuParsers.Beatmaps.Objects.HitObject hitObject){
+        GameObject beat = Instantiate(beatPrefab, beatHolder);
+        beat.name = beat.name + latestSpawnedBeat;
+        beat.transform.position = beatMarker.position + Vector3.forward * ((float)(hitObject.StartTime - songTimer)/100f);
+        beats.Add(beat.transform);
+
+        latestSpawnedBeat += 1;
     }
 
     public void OnSongEnd(){
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using System.IO;
+using System.IO.Compression;
 
 public enum GameState {
     Started = 0x01,
@@ -14,26 +16,126 @@ public class GameManager : MonoBehaviour {
     public static GameState gameState; 
     
     // https://github.com/mrflashstudio/OsuParsers.
-    public string beatmapRepository = @"Assets/Beatmaps/";
-    private static List<OsuParsers.Beatmaps.Beatmap> beatmaps = new List<OsuParsers.Beatmaps.Beatmap>();
+    public static string beatmapRepositoryPath = @"Assets/Beatmaps/";
+    public static string beatmapTempPath = @"Assets/Beatmaps/Temp/";
+
+    private static Dictionary<string, List<OsuParsers.Beatmaps.Beatmap>> beatmapsDictionary = new Dictionary<string, List<OsuParsers.Beatmaps.Beatmap>>();
     
     void Awake(){
         if (instance == null){
             instance = this;
-            DirectoryInfo info = new DirectoryInfo(beatmapRepository);
-            FileInfo[] fileInfo = info.GetFiles("*.osu", SearchOption.AllDirectories);
 
-            foreach(FileInfo file in fileInfo){
-                // print(file.FullName);
-
-                OsuParsers.Beatmaps.Beatmap beatmap = OsuParsers.Decoders.BeatmapDecoder.Decode(file.FullName);
-                beatmaps.Add(beatmap);
+            // Clear out Temp folder.
+            DirectoryInfo tempInfo = new DirectoryInfo(beatmapTempPath);
+            foreach(FileInfo tempFile in tempInfo.GetFiles()){
+                tempFile.Delete();
             }
 
-            StartBeatmap(beatmaps[0]);
+            // Get all osz files.
+            DirectoryInfo info = new DirectoryInfo(beatmapRepositoryPath);
+            FileInfo[] fileInfo = info.GetFiles("*.osz", SearchOption.AllDirectories);
+
+            // Open all osz files.
+            foreach(FileInfo oszFile in fileInfo){
+                ZipArchive zipArchive = ZipFile.OpenRead(oszFile.FullName);
+                List<OsuParsers.Beatmaps.Beatmap> beatmaps = new List<OsuParsers.Beatmaps.Beatmap>();
+                beatmapsDictionary.Add(oszFile.Name, beatmaps);
+
+                // Loop through zipped content.
+                foreach(ZipArchiveEntry entry in zipArchive.Entries){
+
+                    // Osu file, parse to beatmap.
+                    if (entry.FullName.EndsWith(".osu")){
+                        string extractOsuPath = Path.Combine(beatmapTempPath + entry.FullName);
+
+                        entry.ExtractToFile(extractOsuPath);
+
+                        OsuParsers.Beatmaps.Beatmap beatmap = OsuParsers.Decoders.BeatmapDecoder.Decode(extractOsuPath);
+                        beatmaps.Add(beatmap);
+                    }
+
+                    // Mp3 file, assume it's the song..
+                    if (entry.FullName.EndsWith(".mp3")){
+                        string extractOsuPath = Path.Combine(beatmapTempPath + entry.FullName);
+
+                        entry.ExtractToFile(extractOsuPath);
+                    }
+                }
+            }
+
+            StartBeatmap(beatmapsDictionary["24664 DOES - Donten (TV Size).osz"][1]);
         } else {
             Destroy(gameObject);
         }
+    }
+
+    public static IEnumerator GetBeatmapAudioClip(OsuParsers.Beatmaps.Beatmap beatmap, System.Action<AudioClip> callback){
+        DirectoryInfo info = new DirectoryInfo(beatmapTempPath);
+        FileInfo audioFile = info.GetFiles(beatmap.GeneralSection.AudioFilename)[0];
+
+        print("file://" + audioFile.FullName);
+
+        // Licensing issues.
+        // UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip("file://" + audioFile.FullName, AudioType.MPEG);
+
+        UnityWebRequest www = UnityWebRequest.Get("file://" + audioFile.FullName);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError){
+            print(www.error);
+        } else {
+            // AudioClip audioClip = DownloadHandlerAudioClip.GetContent(www);
+            
+            AudioClip audioClip = GetAudioClipFromMP3ByteArray(www.downloadHandler.data);
+            callback(audioClip);
+        }
+    }
+
+    // http://answers.unity.com/answers/632260/view.html
+    // https://github.com/ZaneDubya/MP3Sharp
+    private static AudioClip GetAudioClipFromMP3ByteArray( byte[] in_aMP3Data ){
+        AudioClip l_oAudioClip = null;
+        Stream l_oByteStream = new MemoryStream( in_aMP3Data );
+        MP3Sharp.MP3Stream l_oMP3Stream = new MP3Sharp.MP3Stream( l_oByteStream );
+
+        //Get the converted stream data
+        MemoryStream l_oConvertedAudioData = new MemoryStream();
+        byte[] l_aBuffer = new byte[ 2048 ];
+        int l_nBytesReturned = -1;
+        int l_nTotalBytesReturned = 0;
+
+        while( l_nBytesReturned != 0 )
+        {
+            l_nBytesReturned = l_oMP3Stream.Read( l_aBuffer, 0, l_aBuffer.Length );
+            l_oConvertedAudioData.Write( l_aBuffer, 0, l_nBytesReturned );
+            l_nTotalBytesReturned += l_nBytesReturned;
+        }
+
+        Debug.Log( "MP3 file has " + l_oMP3Stream.ChannelCount + " channels with a frequency of " + l_oMP3Stream.Frequency );
+
+        byte[] l_aConvertedAudioData = l_oConvertedAudioData.ToArray();
+        Debug.Log( "Converted Data has " + l_aConvertedAudioData.Length + " bytes of data" );
+
+        //Convert the byte converted byte data into float form in the range of 0.0-1.0
+        float[] l_aFloatArray = new float[ l_aConvertedAudioData.Length / 2 ];
+
+        for( int i = 0; i < l_aFloatArray.Length; i++ )
+        {
+            if( System.BitConverter.IsLittleEndian )
+            {
+                //Evaluate earlier when pulling from server and/or local filesystem - not needed here
+                //Array.Reverse( l_aConvertedAudioData, i * 2, 2 );
+            }
+            
+            //Yikes, remember that it is SIGNED Int16, not unsigned (spent a bit of time before realizing I screwed this up...)
+            l_aFloatArray[ i ] = (float)( System.BitConverter.ToInt16( l_aConvertedAudioData, i * 2 ) / 32768.0f );
+        }
+
+        //  l_oAudioClip = AudioClip.Create( "MySound", l_aFloatArray.Length, 2, l_oMP3Stream.Frequency, false, false );
+        l_oAudioClip = AudioClip.Create("MySound", l_aFloatArray.Length, 2, l_oMP3Stream.Frequency, false);
+        l_oAudioClip.SetData( l_aFloatArray, 0 );
+
+        return l_oAudioClip;
     }
 
     public void StartBeatmap(OsuParsers.Beatmaps.Beatmap beatmap){
@@ -41,10 +143,7 @@ public class GameManager : MonoBehaviour {
             return;
         }
 
-        BeatmapGame.instance.hitObjects = beatmap.HitObjects;
-        BeatmapGame.instance.songDuration = beatmap.HitObjects[beatmap.HitObjects.Count - 1].EndTime;
-
-        gameState = gameState | GameState.Started;
+        BeatmapGame.instance.StartBeatmap(beatmap);
     }
 
     public static void Stop(){
