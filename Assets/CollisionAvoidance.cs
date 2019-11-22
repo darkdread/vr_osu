8955ef868f3aa7b378b9db0f2b2f79c362baaa67
roycezhang26 <zhangxiaoyin26@gmail.com>
2019-11-22 17:07:34 +0800

    simple collision avoidance

---
+++ Assets/CollisionAvoidance.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAvoidance : MonoBehaviour
+{
+	public Transform target;
+
+	public LayerMask obstacleMask;
+	public float rayLength = 10f;
+	public float speed = 5f;
+	public float rotateSpeed = 8f;
+
+	public bool showDebugRays;
+
+	private Vector3 _velocity;
+	private Vector3 _desiredVelocity;
+
+	private Vector3 _direction;
+	private Vector3 _reflectedDirection;
+	private RaycastHit _hit;
+	private float _penetrationDepth;
+
+	// Update is called once per frame
+	void Update()
+    {
+		_direction = (target.position - transform.position).normalized;
+		//_direction = transform.forward;
+		_velocity = transform.forward * speed * Time.deltaTime;
+		_desiredVelocity = (target.position - transform.position).normalized;
+
+		RaycastHit hit;
+		Physics.Raycast(transform.position, transform.forward, out hit, rayLength, obstacleMask);
+
+		_penetrationDepth = rayLength - hit.distance;
+		print(_penetrationDepth);
+
+		// Change direction.
+		if (hit.collider != null) {
+			_hit = hit;
+			_reflectedDirection = Vector3.Reflect(_direction, hit.normal);
+
+			_direction = (transform.forward + hit.normal * _penetrationDepth).normalized;
+		}
+
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_direction), Time.deltaTime * rotateSpeed);
+		transform.position += _velocity;
+	}
+
+	private void OnDrawGizmos() {
+		if (!showDebugRays) {
+			return;
+		}
+		
+		Gizmos.color = Color.blue;
+		Gizmos.DrawRay(transform.position, _direction * rayLength);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawRay(transform.position, _reflectedDirection * rayLength);
+
+		Gizmos.color = Color.black;
+		Gizmos.DrawRay(_hit.point, _hit.normal * _penetrationDepth);
+	}
+}
---
+++ Assets/Scripts/ImprovedCollisionAvoidance.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImprovedCollisionAvoidance : MonoBehaviour {
+	private RaycastHit hitLeft;
+	private RaycastHit hitRight;
+	private Vector3 hitNormal;
+    public float minimumDistanceToAvoid;
+	public float steerForce;
+	public float rotateSpeed;
+	public float translateSpeed;
+	private Vector3 direction;
+	private Quaternion rotate;
+	private Vector3 leftRayDirection;
+	private Vector3 rightRayDirection;
+	enum Sensor {
+		LEFT,
+		RIGHT
+	};
+	Sensor touch;
+
+	void Update() {
+		leftRayDirection = transform.TransformDirection(new Vector3(-1, 0, 1));
+		rightRayDirection = transform.TransformDirection(new Vector3(1, 0, 1));
+
+		if(Physics.Raycast(transform.position, leftRayDirection, out hitLeft, minimumDistanceToAvoid)) {
+			if(hitLeft.transform != transform) { // Intersection with own collider is omitted
+				touch = Sensor.LEFT;
+			}
+		}
+
+		if(Physics.Raycast(transform.position, rightRayDirection, out hitRight, minimumDistanceToAvoid)) {
+			if(hitRight.transform != transform) { // Intersection with own collider is omitted
+				touch = Sensor.RIGHT;
+			}
+		}
+
+		switch(touch) {
+			case Sensor.LEFT:
+				Vector3 leftHitNormal = hitLeft.normal;
+				Debug.DrawRay(hitLeft.point, leftHitNormal, Color.red);
+				leftHitNormal.y = 0.0f; // Restrict movement in y direction
+				direction = transform.forward + leftHitNormal * steerForce;
+				break;
+			case Sensor.RIGHT:
+				Vector3 rightHitNormal = hitRight.normal;
+				Debug.DrawRay(hitRight.point, rightHitNormal, Color.blue);
+				rightHitNormal.y = 0.0f; // Restrict movement in y direction
+				direction = transform.forward + rightHitNormal * steerForce;
+				break;
+		}
+
+		rotate = Quaternion.LookRotation(direction.normalized);
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotate, Time.deltaTime * rotateSpeed);
+		transform.position += transform.forward * Time.deltaTime * translateSpeed;
+	}
+}
